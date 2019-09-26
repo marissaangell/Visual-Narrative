b132b5a5a2b1d9e011b4316a3d9ebddeba26cd21
Marissa Angell <maa.angell@gmail.com>
2019-09-26 13:50:08 -0600

    Changes so far

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,21 @@
-$("#page_1").click(function(){
+/*$("#page_1").click(function(){
 	$(this).fadeOut();
 	$("#page_1-1").show();
+})*/
+
+$("#mapto_1-1").click(function(){
+	$("#page_1").fadeOut();
+	$("#page_1-1").show();
 })
+
+$("#mapto_1-1-1").click(function(){
+	$("#page_1-1").fadeOut();
+	$("#page_1-1-1").show();
+})
+
+
+
+
+
+
+$('map').imageMapResize();
