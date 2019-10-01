f2aa66b145c1689a77347b58ad8846676b410e70
Marissa Angell <maa.angell@gmail.com>
2019-10-01 14:58:43 -0600

    Changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,22 +12,39 @@ $("#marissa_audio").mouseleave(function(){
 	$("#marissa_1").show();
 })
 
-	//start of marissa path 1
-$("#arch1").click(function(){ 
-	$("#marissa_1").fadeOut();
-	$("#marissa_2-1").show();
-})
 
-$("#arch_to_outside").click(function(){ 
-	$("#marissa_2-1").fadeOut();
-	$("#marissa_2-2").show();
-})
+	//start of marissa path 1
+	$("#arch1").click(function(){ 
+		$("#marissa_1").fadeOut();
+		$("#marissa_2-1").show();
+	})
+
+	$("#arch_to_outside").click(function(){ 
+		$("#marissa_2-1").fadeOut();
+		$("#marissa_3-1").show();
+	})
+
+	$("#").click(function(){ 
+		$("#marissa_3-1").fadeOut();
+		$("#marissa_4-1").show();
+	})
+
 
 	//start of marissa path 2
-$("#arch2").click(function(){
-	$("#marissa_1").fadeOut();
-	$("#marissa_2-2").show();
-})
+	$("#arch2").click(function(){
+		$("#marissa_1").fadeOut();
+		$("#marissa_2-2").show();
+	})
+
+	$("#overgrowth").click(function(){
+		$("#marissa_2-2").fadeOut();
+		$("#marissa_3-2").show();
+	})
+
+	$("#").click(function(){
+		$("#marissa_3-2").fadeOut();
+		$("#marissa_4-2").show();
+	})
 
 
 
