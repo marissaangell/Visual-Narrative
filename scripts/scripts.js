1722ed973d7c8d800aebf8035c23038886205c50
Marissa Angell <maa.angell@gmail.com>
2019-10-01 13:25:20 -0600

    Comment scripts

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,9 +3,9 @@ $("audio").mouseleave(function(){
 })
 
 //Marissa path
-$("#main-door-1").click(function(){
-	$("#homepage").fadeOut();
-	$("#marissa_audio").show();
+$("#main-door-1").click(function(){ //id of map you are clicking
+	$("#homepage").fadeOut(); //id of div you want to hide
+	$("#marissa_audio").show(); //id of div you want to show
 })
 
 $("#marissa_audio").mouseleave(function(){
